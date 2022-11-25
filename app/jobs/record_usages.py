9b44b34a4a55165adb7073c6bb3db1ed96e2beb4
SaintShit <saintshit@proton.me>
2022-11-25 17:14:12 +0330

    Improved bulk update in record_usages job

--- app/jobs/record_usages.py
+++ app/jobs/record_usages.py
@@ -1,22 +1,25 @@
+from operator import attrgetter
+
 from app import scheduler, xray
 from app.db import engine
 from app.db.models import System, User
-
-from sqlalchemy import update
-
-# TODO https://stackoverflow.com/questions/25694234/bulk-update-in-sqlalchemy-core-using-where
+from sqlalchemy import bindparam, update
 
 
 def record_users_usage():
     with engine.connect() as conn:
-        for stat in xray.api.get_users_stats(reset=True):
-            if stat.value == 0:
-                continue
-            conn.execute(
-                update(User)
-                .where(User.username == stat.name)
-                .values(used_traffic=User.used_traffic+stat.value)
-            )
+        params = [
+            {"name": stat.name, "value": stat.value}
+            for stat in filter(attrgetter('value'), xray.api.get_users_stats(reset=True))
+        ]
+        if not params:
+            return
+
+        stmt = update(User). \
+            where(User.username == bindparam('name')). \
+            values(used_traffic=User.used_traffic + bindparam('value'))
+
+        conn.execute(stmt, params)
 
 
 scheduler.add_job(record_users_usage, 'interval', seconds=10)
@@ -24,13 +27,19 @@ scheduler.add_job(record_users_usage, 'interval', seconds=10)
 
 def record_outbounds_usage():
     with engine.connect() as conn:
-        for stat in xray.api.get_outbounds_stats(reset=True):
-            if stat.value == 0:
-                continue
-            conn.execute(
-                update(System)
-                .values({stat.link: getattr(System, stat.link)+stat.value})
-            )
+        params = [
+            {"up": stat.value, "down": 0} if stat.link == "uplink" else {"up": 0, "down": stat.value}
+            for stat in filter(attrgetter('value'), xray.api.get_outbounds_stats(reset=True))
+        ]
+        if not params:
+            return
+
+        stmt = update(System).values(
+            uplink=System.uplink + bindparam('up'),
+            downlink=System.downlink + bindparam('down')
+        )
+
+        conn.execute(stmt, params)
 
 
-scheduler.add_job(record_outbounds_usage, 'interval', seconds=5)
+scheduler.add_job(record_outbounds_usage, 'interval', seconds=2)
