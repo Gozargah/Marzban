8b5e7bb2114bb3376adca0d5af6e1d3780fa834c
SaintShit <saintshit@proton.me>
2022-12-17 11:36:03 +0330

    Add dashboards static temporarily

--- app/dashboard/__init__.py
+++ app/dashboard/__init__.py
@@ -4,18 +4,18 @@ from fastapi.staticfiles import StaticFiles
 from pathlib import Path
 
 path = '/dashboard'
-dist = Path(__file__).parent / 'frontend/dist'
+files = Path(__file__).parent / 'frontend/static'
 
 
 def build(base):
-    proc = subprocess.Popen(['yarn', 'build', '--base', base], cwd=dist.parent)
+    proc = subprocess.Popen(['yarn', 'build', '--base', base,  '--outDir', files], cwd=files.parent)
     return proc.wait()
 
 
-if not dist.is_dir():
+if not files.is_dir():
     build(path)
 
 
 app.mount(path,
-          StaticFiles(directory=dist, html=True),
+          StaticFiles(directory=files, html=True),
           name="dashboard")
