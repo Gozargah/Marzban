423da5ab394683c7271abb1b585941df9d19ff7f
SaintShit <saintshit@proton.me>
2023-01-30 15:30:37 +0330

    add get user command to telegram bot

--- app/telegram/admin.py
+++ app/telegram/admin.py
@@ -1,11 +1,16 @@
-from app import xray
 import math
+from datetime import datetime
+
+from app import xray
 from app.db import GetDB, crud
-from app.models.user import UserStatus
+from app.models.user import UserResponse, UserStatus
 from app.telegram import bot
 from app.utils.system import cpu_usage, memory_usage
 from config import TELEGRAM_ADMIN_ID
+from pytz import UTC
 from telebot.custom_filters import ChatFilter
+from telebot.util import extract_arguments
+from telebot.formatting import escape_markdown
 
 bot.add_custom_filter(ChatFilter())
 
@@ -17,6 +22,9 @@ _\- Get system info_
 
 /restart
 _\- Restart Xray core_
+
+/user \<username\>
+_\- Get user information_
 """
 
 system_text = """⚙️ System statistics
@@ -35,6 +43,22 @@ system_text = """⚙️ System statistics
 *Total Users*: `{total_users}`
 *Active Users*: `{active_users}`
 *Deactive Users*: `{deactive_users}`
+"""
+
+
+get_user_text = """
+*Username*: `{username}`
+*Status*: `{status}`
+*Traffic limit*: `{traffic_limit}`
+*Used traffic*: `{used_traffic}`
+*Expiry date*: `{expires_at}`
+*Created at*: `{created_at}`
+*Proxy protocols*: `{protocols}`
+
+*Subscription link*: `{subscription_url}`
+
+*Links*:
+`{links}`
 """
 
 
@@ -82,3 +106,31 @@ def restart_command(message):
     m = bot.reply_to(message, '🔄 Restarting...')
     xray.core.restart()
     bot.edit_message_text('✅ Xray core restarted.', m.chat.id, m.message_id)
+
+
+@bot.message_handler(commands=['user'], chat_id=[TELEGRAM_ADMIN_ID])
+def user_command(message):
+    username = extract_arguments(message.text)
+    if not username:
+        return bot.reply_to(message, 'Usage: `/user <username>`', parse_mode='MarkdownV2')
+
+    with GetDB() as db:
+        dbuser = crud.get_user(db, username)
+
+        if not dbuser:
+            return bot.reply_to(message, "No user found with this username")
+        user = UserResponse.from_orm(dbuser)
+
+        text = get_user_text.format(
+            username=user.username,
+            status=user.status,
+            traffic_limit=readable_size(user.data_limit) if user.data_limit else '-',
+            used_traffic=readable_size(user.used_traffic),
+            expires_at=datetime.fromtimestamp(user.expire, UTC).strftime('%m/%d/%Y') if user.expire else '-',
+            created_at=user.created_at.strftime('%m/%d/%Y'),
+            protocols=','.join(user.proxies.keys()),
+            subscription_url=escape_markdown(user.subscription_url),
+            links=escape_markdown('\n'.join(user.links))
+        )
+
+    return bot.reply_to(message, text, parse_mode='MarkdownV2')
