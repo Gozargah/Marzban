74320e74c1c6e73917c3bf7da7b3aee447f1153d
Mohammadreza Varasteh <varastehmr@gmail.com>
2023-02-01 22:19:45 +0330

    Add UserUsageResetLogs Model and data_limit_strategy field to user

--- app/db/models.py
+++ app/db/models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from app.db.base import Base
 from app.models.proxy import ProxyTypes
-from app.models.user import UserStatus
+from app.models.user import UserStatus, UserDataLimitResetStrategy
 from sqlalchemy import Column, Integer, BigInteger, String, Enum, JSON, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 
@@ -27,11 +27,21 @@ class User(Base):
     status = Column(Enum(UserStatus), default=UserStatus.active)
     used_traffic = Column(BigInteger, default=0)
     data_limit = Column(BigInteger, nullable=True)
+    data_limit_reset_strategy = Column(Integer,default=UserDataLimitResetStrategy.no_reset)
+    usage_logs = relationship("UserUsageResetLogs", back_populates="user")
     expire = Column(Integer, nullable=True)
     admin_id = Column(Integer, ForeignKey("admins.id"))
     admin = relationship("Admin", back_populates="users")
     created_at = Column(DateTime, default=datetime.utcnow)
 
+class UserUsageResetLogs(Base):
+    __tablename__ = "user_usage_logs"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("User", back_populates="usage_logs")
+    used_traffic_at_reset = Column(BigInteger, nullable=False)
+    reset_at = Column(DateTime, default=datetime.utcnow)
 
 class Proxy(Base):
     __tablename__ = "proxies"
