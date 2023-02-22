4ff0bcce21e10f5d2abee4a2f1254fe3cff32a76
bahram rajabi <bahram.rajabiws@gmail.com>
2023-02-22 16:25:54 +0330

    show traffic & status information for public users in telegram . (#34)

--- app/telegram/admin.py
+++ app/telegram/admin.py
@@ -1,6 +1,5 @@
-import math
+from app.utils.readable import readable_size
 from datetime import datetime
-
 from app import xray
 from app.db import GetDB, crud
 from app.models.user import UserResponse, UserStatus
@@ -60,16 +59,6 @@ get_user_text = """
 *Links*:
 `{links}`
 """
-
-
-def readable_size(size_bytes):
-    if size_bytes == 0:
-        return "0B"
-    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
-    i = int(math.floor(math.log(size_bytes, 1024)))
-    p = math.pow(1024, i)
-    s = round(size_bytes / p, 2)
-    return f'{s} {size_name[i]}'
 
 
 @bot.message_handler(commands=['start', 'help'], chat_id=[TELEGRAM_ADMIN_ID])
---
+++ app/telegram/user.py
@@ -0,0 +1,45 @@
+from app.utils.readable import readable_size
+from datetime import datetime
+from app.db import GetDB, crud
+from app.models.user import UserResponse
+from app.telegram import bot
+from pytz import UTC
+from telebot.custom_filters import ChatFilter
+from telebot.util import extract_arguments
+
+bot.add_custom_filter(ChatFilter())
+
+get_user_text = """
+*Username*: `{username}`
+*Status*: `{status}`
+*Traffic limit*: `{traffic_limit}`
+*Used traffic*: `{used_traffic}`
+*Expiry date*: `{expires_at}`
+*Created at*: `{created_at}`
+*Proxy protocols*: `{protocols}`
+"""
+
+@bot.message_handler(commands=['usage'])
+def usage_command(message):
+    username = extract_arguments(message.text)
+    if not username:
+        return bot.reply_to(message, 'Usage: `/usage <username>`', parse_mode='MarkdownV2')
+
+    with GetDB() as db:
+        dbuser = crud.get_user(db, username)
+
+        if not dbuser:
+            return bot.reply_to(message, "No user found with this username")
+        user = UserResponse.from_orm(dbuser)
+
+        text = get_user_text.format(
+            username=user.username,
+            status=user.status,
+            traffic_limit=readable_size(user.data_limit) if user.data_limit else '-',
+            used_traffic=readable_size(user.used_traffic),
+            expires_at=datetime.fromtimestamp(user.expire, UTC).strftime('%m/%d/%Y') if user.expire else '-',
+            created_at=user.created_at.strftime('%m/%d/%Y'),
+            protocols=','.join(user.proxies.keys())
+        )
+
+    return bot.reply_to(message, text, parse_mode='MarkdownV2')
