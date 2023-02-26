83cadee6e2160c558d8ef989714cdb6f47ffc496
Gozarman <gozarman@proton.me>
2023-02-26 12:16:31 +0330

    feat(frontend): hosts management

--- app/dashboard/src/components/HostsDialog.tsx
+++ app/dashboard/src/components/HostsDialog.tsx
@@ -1,5 +1,5 @@
-import { useDashboard } from "../contexts/DashboardContext";
-import { FC } from "react";
+import { useDashboard, useHosts } from "../contexts/DashboardContext";
+import { FC, useEffect, useRef, useState } from "react";
 import {
   Accordion,
   AccordionButton,
@@ -9,7 +9,10 @@ import {
   Box,
   Button,
   chakra,
+  FormControl,
+  FormErrorMessage,
   HStack,
+  IconButton,
   Input,
   Modal,
   ModalBody,
@@ -18,12 +21,23 @@ import {
   ModalHeader,
   ModalOverlay,
   Text,
+  Tooltip,
+  useToast,
   VStack,
 } from "@chakra-ui/react";
 import { Icon } from "./Icon";
 import { LinkIcon } from "@heroicons/react/24/outline";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import {
+  FormProvider,
+  useFieldArray,
+  useForm,
+  useFormContext,
+} from "react-hook-form";
+import { DeleteIcon } from "./DeleteUserModal";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const ModalIcon = chakra(LinkIcon, {
   baseStyle: {
@@ -32,7 +46,72 @@ const ModalIcon = chakra(LinkIcon, {
   },
 });
 
-const AccordionInbound: FC = () => {
+const hostsSchema = z.record(
+  z.string().min(1),
+  z.array(
+    z.object({
+      remark: z.string().min(1, "Remark is required"),
+      address: z.string().min(1, "Address is required"),
+      port: z
+        .string()
+        .or(z.number())
+        .nullable()
+        .transform((value) => {
+          if (typeof value === "number") return value;
+          if (value !== null && !isNaN(parseInt(value)))
+            return Number(parseInt(value));
+          return null;
+        }),
+      inbound_tag: z.string(),
+    })
+  )
+);
+
+const Error = chakra(FormErrorMessage, {
+  baseStyle: {
+    color: "red.400",
+    display: "block",
+    textAlign: "left",
+    w: "100%",
+  },
+});
+
+type AccordionInboundType = {
+  hostKey: string;
+  isOpen: boolean;
+  toggleAccordion: () => void;
+};
+
+const AccordionInbound: FC<AccordionInboundType> = ({
+  hostKey,
+  isOpen,
+  toggleAccordion,
+}) => {
+  const form = useFormContext<z.infer<typeof hostsSchema>>();
+  const {
+    fields: hosts,
+    append: addHost,
+    remove: removeHost,
+  } = useFieldArray({
+    control: form.control,
+    name: hostKey,
+  });
+  const { errors } = form.formState;
+  console.log(errors);
+  const accordionErrors = errors[hostKey];
+  const handleAddHost = () => {
+    addHost({
+      inbound_tag: hostKey,
+      port: null,
+      address: "",
+      remark: "",
+    });
+  };
+  useEffect(() => {
+    if (accordionErrors && !isOpen) {
+      toggleAccordion();
+    }
+  }, [accordionErrors]);
   return (
     <AccordionItem
       border="1px solid"
@@ -42,7 +121,7 @@ const AccordionInbound: FC = () => {
       p={1}
       w="full"
     >
-      <AccordionButton px={2} borderRadius="3px">
+      <AccordionButton px={2} borderRadius="3px" onClick={toggleAccordion}>
         <Text
           as="span"
           fontWeight="medium"
@@ -52,36 +131,84 @@ const AccordionInbound: FC = () => {
           color="gray.700"
           _dark={{ color: "gray.300" }}
         >
-          VLESS_NO_TLS
+          {hostKey}
         </Text>
         <AccordionIcon />
       </AccordionButton>
       <AccordionPanel px={2} pb={2}>
         <VStack gap={3}>
-          <VStack
-            border="1px solid"
-            _dark={{ borderColor: "gray.600" }}
-            _light={{ borderColor: "gray.200" }}
-            p={2}
-            borderRadius="4px"
-          >
-            <Input size="sm" borderRadius="4px" placeholder="Remark" />
-            <HStack>
-              <Input
-                w="70%"
-                size="sm"
-                borderRadius="4px"
-                placeholder="Address (e.g. google.com)"
-              />
-              <Input w="30%" size="sm" borderRadius="4px" placeholder="8080" />
-            </HStack>
-          </VStack>
+          {hosts.map((host, index) => (
+            <VStack
+              key={index}
+              border="1px solid"
+              _dark={{ borderColor: "gray.600", bg: "#273142" }}
+              _light={{ borderColor: "gray.200", bg: "#fcfbfb" }}
+              p={2}
+              borderRadius="4px"
+            >
+              <HStack w="100%" alignItems="flex-start">
+                <FormControl
+                  isInvalid={
+                    !!(accordionErrors && accordionErrors[index]?.remark)
+                  }
+                >
+                  <Input
+                    {...form.register(hostKey + "." + index + ".remark")}
+                    size="sm"
+                    borderRadius="4px"
+                    placeholder="Remark"
+                  />
+                  {accordionErrors && accordionErrors[index]?.remark && (
+                    <Error>{accordionErrors[index]?.remark?.message}</Error>
+                  )}
+                </FormControl>
+                <Tooltip label="Delete" placement="top">
+                  <IconButton
+                    aria-label="Delete"
+                    size="sm"
+                    colorScheme="red"
+                    variant="ghost"
+                    onClick={removeHost.bind(null, index)}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </Tooltip>
+              </HStack>
+              <HStack alignItems="flex-start">
+                <FormControl
+                  w="70%"
+                  isInvalid={
+                    !!(accordionErrors && accordionErrors[index]?.address)
+                  }
+                >
+                  <Input
+                    size="sm"
+                    borderRadius="4px"
+                    placeholder="Address (e.g. google.com)"
+                    {...form.register(hostKey + "." + index + ".address")}
+                  />
+                  {accordionErrors && accordionErrors[index]?.address && (
+                    <Error>{accordionErrors[index]?.address?.message}</Error>
+                  )}
+                </FormControl>
+                <Input
+                  w="30%"
+                  size="sm"
+                  borderRadius="4px"
+                  placeholder="8080"
+                  type="number"
+                  {...form.register(hostKey + "." + index + ".port")}
+                />
+              </HStack>
+            </VStack>
+          ))}
           <Button
             variant="outline"
             w="full"
             size="sm"
             color=""
             fontWeight={"normal"}
+            onClick={handleAddHost}
           >
             Add host
           </Button>
@@ -92,8 +219,49 @@ const AccordionInbound: FC = () => {
 };
 
 export const HostsDialog: FC = () => {
-  const { isEditingHosts, onEditingHosts } = useDashboard();
-  const onClose = onEditingHosts.bind(null, false);
+  const { isEditingHosts, onEditingHosts, refetchUsers } = useDashboard();
+  const { isLoading, hosts, fetchHosts, isPostLoading, setHosts } = useHosts();
+  const toast = useToast();
+  const [openAccordions, setOpenAccordions] = useState<any>({});
+
+  useEffect(() => {
+    if (isEditingHosts) fetchHosts();
+  }, [isEditingHosts]);
+  const form = useForm<z.infer<typeof hostsSchema>>({
+    resolver: zodResolver(hostsSchema),
+  });
+
+  useEffect(() => {
+    if (hosts && isEditingHosts) {
+      form.reset(hosts);
+    }
+  }, [hosts]);
+
+  const onClose = () => {
+    setOpenAccordions({});
+    onEditingHosts(false);
+  };
+  const handleFormSubmit = (hosts: z.infer<typeof hostsSchema>) => {
+    setHosts(hosts).then(() => {
+      toast({
+        title: `Hosts saved successfully`,
+        status: "success",
+        isClosable: true,
+        position: "top",
+        duration: 3000,
+      });
+      refetchUsers();
+    });
+  };
+
+  const toggleAccordion = (index: number) => {
+    if (openAccordions[String(index)]) {
+      delete openAccordions[String(index)];
+    } else openAccordions[String(index)] = {};
+
+    setOpenAccordions({ ...openAccordions });
+  };
+
   return (
     <Modal isOpen={isEditingHosts} onClose={onClose}>
       <ModalOverlay bg="blackAlpha.300" backdropFilter="blur(10px)" />
@@ -105,27 +273,55 @@ export const HostsDialog: FC = () => {
         </ModalHeader>
         <ModalCloseButton mt={3} />
         <ModalBody w="440px" pb={3} pt={3}>
-          <Text mb={3} opacity={0.8} fontSize="sm">
-            Using this setting, you are able to assign specific address for each
-            inbound.
-          </Text>
-          <Accordion w="full" allowToggle allowMultiple>
-            <VStack w="full">
-              <AccordionInbound />
-              <AccordionInbound />
-            </VStack>
-          </Accordion>
-          <HStack justifyContent="flex-end" py={2}>
-            <Button
-              variant="solid"
-              mt="2"
-              colorScheme="primary"
-              size="sm"
-              px={5}
-            >
-              Apply
-            </Button>
-          </HStack>
+          <FormProvider {...form}>
+            <form onSubmit={form.handleSubmit(handleFormSubmit)}>
+              <Text mb={3} opacity={0.8} fontSize="sm">
+                Using this setting, you are able to assign specific address for
+                each inbound.
+              </Text>
+              {isLoading && "loading..."}
+              {!isLoading &&
+                hosts &&
+                (Object.keys(hosts).length > 0 ? (
+                  <Accordion
+                    w="full"
+                    allowToggle
+                    allowMultiple
+                    index={Object.keys(openAccordions).map((i) => parseInt(i))}
+                  >
+                    <VStack w="full">
+                      {Object.keys(hosts).map((hostKey, index) => {
+                        return (
+                          <AccordionInbound
+                            toggleAccordion={() => toggleAccordion(index)}
+                            isOpen={openAccordions[String(index)]}
+                            key={hostKey}
+                            hostKey={hostKey}
+                          />
+                        );
+                      })}
+                    </VStack>
+                  </Accordion>
+                ) : (
+                  "No inbound found. Please check your Xray config file."
+                ))}
+
+              <HStack justifyContent="flex-end" py={2}>
+                <Button
+                  variant="solid"
+                  mt="2"
+                  type="submit"
+                  colorScheme="primary"
+                  size="sm"
+                  px={5}
+                  isLoading={isPostLoading}
+                  disabled={isPostLoading}
+                >
+                  Apply
+                </Button>
+              </HStack>
+            </form>
+          </FormProvider>
         </ModalBody>
       </ModalContent>
     </Modal>
