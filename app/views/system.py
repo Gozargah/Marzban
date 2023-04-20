68a3a616d4026d3e124bef2dbf320811c07a6656
GovFvck <govfvck@gmail.com>
2023-04-20 20:12:44 +0330

    Improve server statistics
    add `cpu_cores` and `cpu_usage` to get number of cpu cores and percentage of cpu usage
    add `incoming_bandwidth_speed` and `outgoing_bandwidth_speed` to show real time data about system's bandwidth
    	add utility functions to monitor outgoing and incoming bytes/packets
    	realtime data is available in `rt_bw` inside the `utils/system.py`, it will be updated every second

--- app/views/system.py
+++ app/views/system.py
@@ -9,25 +9,31 @@ from app.models.proxy import ProxyHost, ProxyInbound, ProxyTypes
 from app.models.system import SystemStats
 from app.models.user import UserStatus
 from app.utils.store import XrayStore
-from app.utils.system import memory_usage
+from app.utils.system import memory_usage, cpu_usage, realtime_bandwith
 
 
 @app.get("/api/system", tags=["System"], response_model=SystemStats)
 def get_system_stats(db: Session = Depends(get_db), admin: Admin = Depends(Admin.get_current)):
     mem = memory_usage()
+    cpu = cpu_usage()
     system = crud.get_system_usage(db)
     dbadmin: Union[Admin, None] = crud.get_admin(db, admin.username)
 
     total_user = crud.get_users_count(db, admin=dbadmin if not admin.is_sudo else None)
     users_active = crud.get_users_count(db, status=UserStatus.active, admin=dbadmin if not admin.is_sudo else None)
+    realtime_bandwidth_stats = realtime_bandwith()
 
     return SystemStats(
         mem_total=mem.total,
         mem_used=mem.used,
+        cpu_cores=cpu.cores,
+        cpu_usage=cpu.percent,
         total_user=total_user,
         users_active=users_active,
         incoming_bandwidth=system.uplink,
         outgoing_bandwidth=system.downlink,
+        incoming_bandwidth_speed=realtime_bandwidth_stats.incoming_bytes,
+        outgoing_bandwidth_speed=realtime_bandwidth_stats.outgoing_bytes,
     )
 
 
