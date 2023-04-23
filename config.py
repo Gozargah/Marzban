6ff55a143a6eb2e004b39cf83e78e474a6826bbb
SaintShit <saintshit@proton.me>
2023-04-24 02:57:31 +0330

    default values for SUDO_USERNAME and SUDO_PASSWORD

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ CUSTOM_TEMPLATES_DIRECTORY = config("CUSTOM_TEMPLATES_DIRECTORY", default=None)
 CLASH_SUBSCRIPTION_TEMPLATE = config("CLASH_SUBSCRIPTION_TEMPLATE", default="clash/default.yml")
 
 # USERNAME: PASSWORD
-SUDOERS = {config("SUDO_USERNAME"): config("SUDO_PASSWORD")} if config("SUDO_USERNAME", None) else {}
+SUDOERS = {config("SUDO_USERNAME", "admin"): config("SUDO_PASSWORD", "admin")}
 
 
 WEBHOOK_ADDRESS = config("WEBHOOK_ADDRESS", default=None)
