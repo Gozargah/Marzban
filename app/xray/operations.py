500379f637d1d01942708d4e2a7101a5201844dc
codetypes <codetypes@gmail.com>
2023-06-07 20:13:08 +0800

    add or remove user in concurrency
    if many nodes and many inbounds, it spend so much time in test node.connected

--- app/xray/operations.py
+++ app/xray/operations.py
@@ -1,4 +1,5 @@
 from sqlalchemy.exc import SQLAlchemyError
+from threading import Thread
 
 from app import logger, xray
 from app.db import GetDB
@@ -7,52 +8,88 @@ from app.db import crud
 from app.db.models import Node as DBNode
 from app.models.node import NodeStatus
 from app.models.user import UserResponse
+from app.models.proxy import ProxyTypes
 from app.utils.concurrency import threaded_function
 from app.xray.node import XRayNode
 from xray_api.types.account import XTLSFlows
 
+def add_inbound_user(dbuser: DBUser, node_id: int, inbound_tag: str, proxy_type: ProxyTypes):
+    user = UserResponse.from_orm(dbuser)
+    inbound = xray.config.inbounds_by_tag.get(inbound_tag, {})
+    try:
+        proxy_settings = user.proxies[proxy_type].dict(no_obj=True)
+    except KeyError:
+        pass
+    account = proxy_type.account_model(email=f"{dbuser.id}.{dbuser.username}", **proxy_settings)
+
+    # XTLS currently only supports transmission methods of TCP and mKCP
+    if inbound.get('network', 'tcp') not in ('tcp', 'kcp') and getattr(account, 'flow', None):
+        account.flow = XTLSFlows.NONE
+
+    if node_id == 0:
+        try:
+            xray.api.add_inbound_user(tag=inbound_tag, user=account)
+        except (xray.exc.EmailExistsError, xray.exc.ConnectionError):
+            pass
+    else:
+        try:
+            xray.nodes[node_id].api.add_inbound_user(tag=inbound_tag, user=account)
+        except (xray.exc.EmailExistsError, xray.exc.ConnectionError):
+            pass
 
 def add_user(dbuser: DBUser):
     user = UserResponse.from_orm(dbuser)
 
+    threads: list[Thread] = []
+
     for proxy_type, inbound_tags in user.inbounds.items():
         for inbound_tag in inbound_tags:
-            inbound = xray.config.inbounds_by_tag.get(inbound_tag, {})
+            job_thread = Thread(target=add_inbound_user, args=(dbuser, 0, inbound_tag, proxy_type), daemon=True)
+            job_thread.start()
+            threads.append(job_thread)
 
-            try:
-                proxy_settings = user.proxies[proxy_type].dict(no_obj=True)
-            except KeyError:
-                pass
-            account = proxy_type.account_model(email=f"{dbuser.id}.{dbuser.username}", **proxy_settings)
-
-            # XTLS currently only supports transmission methods of TCP and mKCP
-            if inbound.get('network', 'tcp') not in ('tcp', 'kcp') and getattr(account, 'flow', None):
-                account.flow = XTLSFlows.NONE
-
-            try:
-                xray.api.add_inbound_user(tag=inbound_tag, user=account)
-            except (xray.exc.EmailExistsError, xray.exc.ConnectionError):
-                pass
-            for node in xray.nodes.values():
-                if node.connected and node.started:
-                    try:
-                        node.api.add_inbound_user(tag=inbound_tag, user=account)
-                    except (xray.exc.EmailExistsError, xray.exc.ConnectionError):
-                        pass
+    for node_id, node in xray.nodes.copy().items():
+        if node.connected and node.started:
+            for proxy_type, inbound_tags in user.inbounds.items():
+                for inbound_tag in inbound_tags:
+                    job_thread = Thread(target=add_inbound_user, args=(dbuser, node_id, inbound_tag, proxy_type), daemon=True)
+                    job_thread.start()
+                    threads.append(job_thread)
+    
+    for job_thread in threads:
+        job_thread.join()
 
 
-def remove_user(dbuser: DBUser):
-    for inbound_tag in xray.config.inbounds_by_tag:
+def remove_inbound_user(dbuser: DBUser, node_id: int, inbound_tag: str):
+    if node_id == 0:
         try:
             xray.api.remove_inbound_user(tag=inbound_tag, email=f"{dbuser.id}.{dbuser.username}")
         except (xray.exc.EmailNotFoundError, xray.exc.ConnectionError):
             pass
-        for node in xray.nodes.values():
-            if node.connected and node.started:
-                try:
-                    node.api.remove_inbound_user(tag=inbound_tag, email=f"{dbuser.id}.{dbuser.username}")
-                except (xray.exc.EmailNotFoundError, xray.exc.ConnectionError):
-                    pass
+    else:
+        try:
+            xray.nodes[node_id].api.remove_inbound_user(tag=inbound_tag, email=f"{dbuser.id}.{dbuser.username}")
+        except (xray.exc.EmailNotFoundError, xray.exc.ConnectionError):
+            pass
+
+def remove_user(dbuser: DBUser):
+    threads: list[Thread] = []
+
+    for inbound_tag in xray.config.inbounds_by_tag:
+        job_thread = Thread(target=remove_inbound_user, args=(dbuser, 0, inbound_tag))
+        job_thread.start()
+        threads.append(job_thread)
+        
+    for node_id, node in xray.nodes.copy().items():
+        if node.connected and node.started:
+            for inbound_tag in xray.config.inbounds_by_tag:
+                job_thread = Thread(target=remove_inbound_user, args=(dbuser, node_id, inbound_tag))
+                job_thread.start()
+                threads.append(job_thread)
+
+    for job_thread in threads:
+        job_thread.join()
+
 
 
 def remove_node(node_id: int):
