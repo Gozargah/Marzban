91cc784a36622a91a20ce82615b7b3644d7fa284
Muhammad <ashouri.amir81@gmail.com>
2023-06-14 01:47:37 +0330

    Revert "Delete config.py"
    This reverts commit be6c2fba9cbbebbde60cb5ad2e57f92f44960476.

---
+++ config.py
@@ -0,0 +1,56 @@
+from decouple import config
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = config(
+    "SQLALCHEMY_DATABASE_URL", default="sqlite:///db.sqlite3")
+
+UVICORN_HOST = config("UVICORN_HOST", default="0.0.0.0")
+UVICORN_PORT = config("UVICORN_PORT", cast=int, default=8000)
+UVICORN_UDS = config("UVICORN_UDS", default=None)
+UVICORN_SSL_CERTFILE = config("UVICORN_SSL_CERTFILE", default=None)
+UVICORN_SSL_KEYFILE = config("UVICORN_SSL_KEYFILE", default=None)
+
+
+DEBUG = config("DEBUG", default=False, cast=bool)
+DOCS = config("DOCS", default=False, cast=bool)
+
+VITE_BASE_API = f"http://127.0.0.1:{UVICORN_PORT}/api/" \
+    if DEBUG and config("VITE_BASE_API", default="/api/") == "/api/" \
+    else config("VITE_BASE_API", default="/api/")
+
+XRAY_JSON = config("XRAY_JSON", default="./xray_config.json")
+XRAY_FALLBACKS_INBOUND_TAG = config("XRAY_FALLBACKS_INBOUND_TAG", cast=str, default="") or config(
+    "XRAY_FALLBACK_INBOUND_TAG", cast=str, default="")
+XRAY_EXECUTABLE_PATH = config(
+    "XRAY_EXECUTABLE_PATH", default="/usr/local/bin/xray")
+XRAY_ASSETS_PATH = config("XRAY_ASSETS_PATH", default="/usr/local/share/xray")
+XRAY_EXCLUDE_INBOUND_TAGS = config(
+    "XRAY_EXCLUDE_INBOUND_TAGS", default='').split()
+XRAY_SUBSCRIPTION_URL_PREFIX = config(
+    "XRAY_SUBSCRIPTION_URL_PREFIX", default="").strip("/")
+
+
+TELEGRAM_API_TOKEN = config("TELEGRAM_API_TOKEN", default=None)
+TELEGRAM_ADMIN_ID = config("TELEGRAM_ADMIN_ID", cast=int, default=0)
+TELEGRAM_PROXY_URL = config("TELEGRAM_PROXY_URL", default=None)
+
+JWT_ACCESS_TOKEN_EXPIRE_MINUTES = config(
+    "JWT_ACCESS_TOKEN_EXPIRE_MINUTES", cast=int, default=1440)
+
+CUSTOM_TEMPLATES_DIRECTORY = config("CUSTOM_TEMPLATES_DIRECTORY", default=None)
+CLASH_SUBSCRIPTION_TEMPLATE = config(
+    "CLASH_SUBSCRIPTION_TEMPLATE", default="clash/default.yml")
+SUBSCRIPTION_PAGE_TEMPLATE = config(
+    "SUBSCRIPTION_PAGE_TEMPLATE", default="subscription/index.html")
+HOME_PAGE_TEMPLATE = config("HOME_PAGE_TEMPLATE", default="home/index.html")
+
+# USERNAME: PASSWORD
+SUDOERS = {config("SUDO_USERNAME"): config("SUDO_PASSWORD")} \
+    if config("SUDO_USERNAME", default='') and config("SUDO_PASSWORD", default='') \
+    else {}
+
+
+WEBHOOK_ADDRESS = config("WEBHOOK_ADDRESS", default=None)
+WEBHOOK_SECRET = config("WEBHOOK_SECRET", default=None)
