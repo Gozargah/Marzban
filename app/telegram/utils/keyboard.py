e292a7f45de0ede6150decffbe517c0322b6d499
Mohammad Chenarani <115184760+mmdchnar@users.noreply.github.com>
2023-07-17 23:57:08 +0330

    Add charge buttons and some style

--- app/telegram/utils/keyboard.py
+++ app/telegram/utils/keyboard.py
@@ -24,10 +24,8 @@ class BotKeyboard:
             types.InlineKeyboardButton(text='♻️ Restart Xray', callback_data='restart'),
         )
         keyboard.add(
-            types.InlineKeyboardButton(text='👥 Users', callback_data='users:1')
-        )
-        keyboard.add(
-            types.InlineKeyboardButton(text='➕ Create User', callback_data='add_user')
+            types.InlineKeyboardButton(text='➕ Create User', callback_data='add_user'),
+            types.InlineKeyboardButton(text='👥 Users', callback_data='users:1'),
         )
         keyboard.add(
             types.InlineKeyboardButton(text='➕ Create User from Template', callback_data='template_add_user')
@@ -35,65 +33,63 @@ class BotKeyboard:
         return keyboard
 
     @staticmethod
-    def templates_menu(templates: Dict[str, int]):
+    def templates_menu(templates: Dict[str, int], username: str = None):
         keyboard = types.InlineKeyboardMarkup()
 
         for chunk in chunk_dict(templates):
             row = []
-            for name, id in chunk.items():
+            for name, _id in chunk.items():
                 row.append(
                     types.InlineKeyboardButton(
                         text=name,
-                        callback_data=f"template_add_user:{id}"
+                        callback_data=f'template_charge:{_id}:{username}' if username else f"template_add_user:{_id}"
                     )
                 )
             keyboard.add(*row)
 
         keyboard.add(
             types.InlineKeyboardButton(
-                text='Back',
-                callback_data='cancel'
+                text='🔙 Back',
+                callback_data=f'user:{username}' if username else 'cancel'
             )
         )
 
         return keyboard
 
+
     @staticmethod
-    def user_menu(user_info, with_back: bool = True, page: int = 1, view_user: bool = False):
+    def user_menu(user_info, with_back: bool = True, page: int = 1):
         keyboard = types.InlineKeyboardMarkup()
-        if view_user:
-            keyboard.add(
-                types.InlineKeyboardButton(
-                    text='View User',
-                    callback_data=f"user:{user_info['username']}:1"
-                )
-            )
         keyboard.add(
             types.InlineKeyboardButton(
-                text='Suspend User' if user_info['status'] == 'active' else 'Activate User',
+                text='❌ Disable' if user_info['status'] == 'active' else '✅ Activate',
                 callback_data=f"{'suspend' if user_info['status'] == 'active' else 'activate'}:{user_info['username']}"
             ),
             types.InlineKeyboardButton(
-                text='Delete User',
+                text='🗑 Delete',
                 callback_data=f"delete:{user_info['username']}"
             ),
         )
         keyboard.add(
             types.InlineKeyboardButton(
-                text='Show Links',
+                text='📡 Links',
                 callback_data=f"links:{user_info['username']}"
+            ),
+            types.InlineKeyboardButton(
+                text='✏️ Edit',
+                callback_data=f"edit:{user_info['username']}"
             )
         )
         keyboard.add(
             types.InlineKeyboardButton(
-                text='Edit User',
-                callback_data=f"edit:{user_info['username']}"
+                text='♻️ Charge',
+                callback_data=f"charge:{user_info['username']}"
             )
         )
         if with_back:
             keyboard.add(
                 types.InlineKeyboardButton(
-                    text='Back',
+                    text='🔙 Back',
                     callback_data=f'users:{page}'
                 )
             )
@@ -105,13 +101,13 @@ class BotKeyboard:
 
         keyboard.add(
             types.InlineKeyboardButton(
-                text="Generate Qr code",
+                text="🖼 Qr code",
                 callback_data=f'genqr:{username}'
             )
         )
         keyboard.add(
             types.InlineKeyboardButton(
-                text='Back',
+                text='🔙 Back',
                 callback_data=f'user:{username}'
             )
         )
@@ -128,6 +124,26 @@ class BotKeyboard:
             types.InlineKeyboardButton(
                 text='No',
                 callback_data=f"cancel"
+            )
+        )
+        return keyboard
+
+    @staticmethod
+    def charge_add_or_reset(username: str, template_id: int):
+        keyboard = types.InlineKeyboardMarkup()
+        keyboard.add(
+            types.InlineKeyboardButton(
+                text='🔰 Add to current',
+                callback_data=f"confirm:charge_add:{username}:{template_id}"
+            ),
+            types.InlineKeyboardButton(
+                text='♻️ Reset',
+                callback_data=f"confirm:charge_reset:{username}:{template_id}"
+            ))
+        keyboard.add(
+            types.InlineKeyboardButton(
+                text="Cancel",
+                callback_data=f'user:{username}'
             )
         )
         return keyboard
@@ -193,7 +209,7 @@ class BotKeyboard:
                 )
         keyboard.add(
             types.InlineKeyboardButton(
-                text='Back',
+                text='🔙 Back',
                 callback_data='cancel'
             )
         )
@@ -203,59 +219,60 @@ class BotKeyboard:
     def select_protocols(selected_protocols: Dict[str, List[str]],
                          action: Literal["edit", "create", "create_from_template"],
                          username: str = None,
-                         data_limit: int = None,
+                         data_limit: float = None,
                          expire_date: dt = None):
         keyboard = types.InlineKeyboardMarkup()
 
-        if action in ["edit", "create_from_template"]:
+        if action == "edit":
             keyboard.add(
                 types.InlineKeyboardButton(
                     text="⚠️ Data Limit:",
-                    callback_data=f"help_edit:data"
+                    callback_data=f"help_edit"
                 )
             )
             keyboard.add(
                 types.InlineKeyboardButton(
                     text=f"{readable_size(data_limit) if data_limit else 'Unlimited'}",
-                    callback_data=f"help_edit:data"
+                    callback_data=f"help_edit"
                 ),
                 types.InlineKeyboardButton(
-                    text="✏️Edit",
+                    text="✏️ Edit",
                     callback_data=f"edit_user:{username}:data"
                 )
             )
             keyboard.add(
                 types.InlineKeyboardButton(
                     text="📅 Expire Date:",
-                    callback_data=f"help_edit:expire"
+                    callback_data=f"help_edit"
                 )
             )
             keyboard.add(
                 types.InlineKeyboardButton(
                     text=f"{expire_date.strftime('%Y-%m-%d') if expire_date else 'Never'}",
-                    callback_data=f"help_edit:expire"
+                    callback_data=f"help_edit"
                 ),
                 types.InlineKeyboardButton(
-                    text="✏️Edit",
+                    text="✏️ Edit",
                     callback_data=f"edit_user:{username}:expire"
                 )
             )
 
-        for protocol, inbounds in xray.config.inbounds_by_protocol.items():
-            keyboard.add(
-                types.InlineKeyboardButton(
-                    text=f"🌐 {protocol.upper()} {'✅' if protocol in selected_protocols else '❌'}",
-                    callback_data=f'select_protocol:{protocol}:{action}'
-                )
-            )
-            if protocol in selected_protocols:
-                for inbound in inbounds:
-                    keyboard.add(
-                        types.InlineKeyboardButton(
-                            text=f"«{inbound['tag']}» {'✅' if inbound['tag'] in selected_protocols[protocol] else '❌'}",
-                            callback_data=f'select_inbound:{inbound["tag"]}:{action}'
-                        )
+        if action != 'create_from_template':
+            for protocol, inbounds in xray.config.inbounds_by_protocol.items():
+                keyboard.add(
+                    types.InlineKeyboardButton(
+                        text=f"🌐 {protocol.upper()} {'✅' if protocol in selected_protocols else '❌'}",
+                        callback_data=f'select_protocol:{protocol}:{action}'
                     )
+                )
+                if protocol in selected_protocols:
+                    for inbound in inbounds:
+                        keyboard.add(
+                            types.InlineKeyboardButton(
+                                text=f"«{inbound['tag']}» {'✅' if inbound['tag'] in selected_protocols[protocol] else '❌'}",
+                                callback_data=f'select_inbound:{inbound["tag"]}:{action}'
+                            )
+                        )
 
         keyboard.add(
             types.InlineKeyboardButton(
@@ -263,18 +280,11 @@ class BotKeyboard:
                 callback_data='confirm:edit_user' if action == "edit" else 'confirm:add_user'
             )
         )
-        if action == "edit":
-            keyboard.add(
-                types.InlineKeyboardButton(
-                    text='Cancel',
-                    callback_data=f'user:{username}'
-                )
+        keyboard.add(
+            types.InlineKeyboardButton(
+                text='Cancel',
+                callback_data=f'user:{username}' if action == "edit" else 'cancel'
             )
-        else:
-            keyboard.add(
-                types.InlineKeyboardButton(
-                    text='Cancel',
-                    callback_data='cancel'
-                )
-            )
+        )
+
         return keyboard
