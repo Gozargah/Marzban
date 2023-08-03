1cbc17bc80ad5b8b01bcfb8a0c04fdf749687034
Random Guy <50927468+M03ED@users.noreply.github.com>
2023-08-03 17:40:16 +0330

    Update __init__.py
    user_last_update changing function

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -27,7 +27,7 @@ from .crud import (create_admin, create_user, get_admin, get_admins,  # noqa
                    get_jwt_secret_key, get_or_create_inbound, get_system_usage,
                    get_user, get_user_by_id, get_users, get_users_count,
                    remove_admin, remove_user, update_admin, update_user,
-                   update_user_status)
+                   update_user_status, user_last_update)
 from .models import JWT, System, User  # noqa
 
 __all__ = [
@@ -47,6 +47,7 @@ __all__ = [
     "update_admin",
     "remove_admin",
     "get_admins",
+    "user_last_update",
 
     "GetDB",
     "get_db",
