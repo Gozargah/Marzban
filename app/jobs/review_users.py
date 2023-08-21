d5db22fe1b09921c21c922a66686ee7f95d97904
govfvck <govfvck@proton.me>
2023-08-21 20:28:08 +0330

    Bugfix: don't send reminders when the created user's expire duration is less than

--- app/jobs/review_users.py
+++ app/jobs/review_users.py
@@ -13,29 +13,30 @@ from config import (NOTIFY_DAYS_LEFT, NOTIFY_REACHED_USAGE_PERCENT,
 
 
 def review():
-    now = datetime.utcnow().timestamp()
+    now = datetime.utcnow()
     with GetDB() as db:
         for user in get_users(db, status=UserStatus.active):
 
             limited = user.data_limit and user.used_traffic >= user.data_limit
-            expired = user.expire and user.expire <= now
+            expired = user.expire and user.expire <= now.timestamp()
             if limited:
                 status = UserStatus.limited
             elif expired:
                 status = UserStatus.expired
             else:
-                if WEBHOOK_ADDRESS:
-                    if user.data_limit and (
-                            usage_percent := calculate_usage_percent(user.used_traffic, user.data_limit)) >= NOTIFY_REACHED_USAGE_PERCENT:
-                        if not get_notification_reminder(db, user.id, ReminderType.data_usage):
-                            report.data_usage_percent_reached(
-                                db, usage_percent, UserResponse.from_orm(user),
-                                user.id, user.expire)
-                    if user.expire and (expire_days := calculate_expiration_days(user.expire)) <= NOTIFY_DAYS_LEFT:
-                        if not get_notification_reminder(db, user.id, ReminderType.expiration_date):
-                            report.expire_days_reached(
-                                db, expire_days, UserResponse.from_orm(user),
-                                user.id, user.expire)
+                if not WEBHOOK_ADDRESS:
+                    continue
+                if user.data_limit and (
+                        usage_percent := calculate_usage_percent(user.used_traffic, user.data_limit)) >= NOTIFY_REACHED_USAGE_PERCENT:
+                    if not get_notification_reminder(db, user.id, ReminderType.data_usage):
+                        report.data_usage_percent_reached(
+                            db, usage_percent, UserResponse.from_orm(user),
+                            user.id, user.expire)
+                if user.expire and ((now - user.created_at).days >= NOTIFY_DAYS_LEFT) and (expire_days := calculate_expiration_days(user.expire)) <= NOTIFY_DAYS_LEFT:
+                    if not get_notification_reminder(db, user.id, ReminderType.expiration_date):
+                        report.expire_days_reached(
+                            db, expire_days, UserResponse.from_orm(user),
+                            user.id, user.expire)
                 continue
 
             xray.operations.remove_user(user)
