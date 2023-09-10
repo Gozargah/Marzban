810852b3d6ccec58f593e323e12fb69923177207
Gozarman <gozarman@proton.me>
2023-09-10 12:54:15 +0330

    feat: russian language added to menu

--- app/dashboard/src/components/Language.tsx
+++ app/dashboard/src/components/Language.tsx
@@ -59,6 +59,13 @@ export const Language: FC<HeaderProps> = ({ actions }) => {
         >
           简体中文
         </MenuItem>
+        <MenuItem
+          maxW="100px"
+          fontSize="sm"
+          onClick={() => changeLanguage("ru")}
+        >
+          русский
+        </MenuItem>
       </MenuList>
     </Menu>
   );
--- app/dashboard/src/locales/i18n.ts
+++ app/dashboard/src/locales/i18n.ts
@@ -1,13 +1,14 @@
 import { joinPaths } from "@remix-run/router";
+
+import fa from "date-fns/locale/fa-IR";
+import ru from "date-fns/locale/ru";
+import zh from "date-fns/locale/zh-CN";
 import dayjs from "dayjs";
-import "dayjs/locale/zh-cn";
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import HttpApi from "i18next-http-backend";
+import { registerLocale } from "react-datepicker";
 import { initReactI18next } from "react-i18next";
-import { registerLocale } from  "react-datepicker";
-import es from 'date-fns/locale/es';
-import zh from 'date-fns/locale/zh-CN'
 
 declare module "i18next" {
   interface CustomTypeOptions {
@@ -19,33 +20,37 @@ i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .use(HttpApi)
-  .init({
-    debug: import.meta.env.NODE_ENV === "development",
-    returnNull: false,
-    fallbackLng: "en",
-    interpolation: {
-      escapeValue: false,
+  .init(
+    {
+      debug: import.meta.env.NODE_ENV === "development",
+      returnNull: false,
+      fallbackLng: "en",
+      interpolation: {
+        escapeValue: false,
+      },
+      react: {
+        useSuspense: false,
+      },
+      load: "languageOnly",
+      detection: {
+        caches: ["localStorage", "sessionStorage", "cookie"],
+      },
+      backend: {
+        loadPath: joinPaths([import.meta.env.BASE_URL, `locales/{{lng}}.json`]),
+      },
     },
-    react: {
-      useSuspense: false,
-    },
-    load: "languageOnly",
-    detection: {
-      caches: ["localStorage", "sessionStorage", "cookie"],
-    },
-    backend: {
-      loadPath: joinPaths([import.meta.env.BASE_URL, `locales/{{lng}}.json`]),
-    },
-  }, function(err, t) {
-    dayjs.locale(i18n.language);
-  });
+    function (err, t) {
+      dayjs.locale(i18n.language);
+    }
+  );
 
 i18n.on("languageChanged", (lng) => {
   dayjs.locale(lng);
 });
 
 // DataPicker
-registerLocale('es', es);
-registerLocale('zh-cn', zh);
+registerLocale("zh-cn", zh);
+registerLocale("ru", ru);
+registerLocale("fa", fa);
 
 export default i18n;
