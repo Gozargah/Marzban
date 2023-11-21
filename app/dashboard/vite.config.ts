679a52fdcad7b247da7a3701f501effa5e6edb2c
Gozarman <120019621+gozarman@users.noreply.github.com>
2023-11-21 22:27:02 +0000

    chore(frontend): upgrade vite

--- app/dashboard/vite.config.ts
+++ app/dashboard/vite.config.ts
@@ -1,7 +1,7 @@
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig, splitVendorChunkPlugin } from "vite";
 import svgr from "vite-plugin-svgr";
-import { visualizer } from "rollup-plugin-visualizer";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
