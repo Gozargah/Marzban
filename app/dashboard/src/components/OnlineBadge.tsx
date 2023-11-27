cd33fe9a4c0e143b309a5a3ae88432faf06207e3
Gozarman <120019621+gozarman@users.noreply.github.com>
2023-11-27 09:16:35 +0000

    fix(frontend): user activity badge colors

--- app/dashboard/src/components/OnlineBadge.tsx
+++ app/dashboard/src/components/OnlineBadge.tsx
@@ -1,4 +1,4 @@
-import { Box, Tooltip } from "@chakra-ui/react";
+import { Box, BoxProps, Tooltip } from "@chakra-ui/react";
 import { FC } from "react";
 import { relativeExpiryDate } from "utils/dateFormatter";
 
@@ -6,7 +6,7 @@ type UserStatusProps = {
   lastOnline?: string | null;
 };
 
-const convertDateFormat = (
+export const convertDateFormat = (
   lastOnline: string | null | undefined
 ): number | null => {
   if (!lastOnline) {
@@ -35,6 +35,25 @@ export const getRelativeLastOnlineAt = ({ lastOnline }: UserStatusProps) => {
     : dateInfo.time;
 };
 
+const statusStyles: Record<"online" | "offline" | "not_connected", BoxProps> = {
+  online: {
+    bg: "green.500",
+  },
+  offline: {
+    bg: "gray.400",
+    _dark: {
+      bg: "gray.600",
+    },
+  },
+  not_connected: {
+    border: "1px solid",
+    borderColor: "gray.400",
+    _dark: {
+      borderColor: "gray.600",
+    },
+  },
+};
+
 export const OnlineBadge: FC<UserStatusProps> = ({ lastOnline }) => {
   const currentTimeInSeconds = Math.floor(Date.now() / 1000);
   const unixTime = convertDateFormat(lastOnline);
@@ -43,18 +62,19 @@ export const OnlineBadge: FC<UserStatusProps> = ({ lastOnline }) => {
     ? currentTimeInSeconds - unixTime
     : Infinity;
 
+  const status =
+    typeof lastOnline === "undefined" || lastOnline === null
+      ? "not_connected"
+      : timeDifferenceInSeconds > 0 && timeDifferenceInSeconds <= 60
+      ? "online"
+      : "offline";
+
   return (
     <Tooltip
       label={getRelativeLastOnlineAt({ lastOnline: lastOnline ?? null })}
       placement="top"
     >
-      {typeof lastOnline === "undefined" || lastOnline === null ? (
-        <Box h={3} w={3} rounded="full" bg="yellow.500" />
-      ) : timeDifferenceInSeconds > 0 && timeDifferenceInSeconds <= 60 ? (
-        <Box h={3} w={3} rounded="full" bg="green.500" />
-      ) : (
-        <Box h={3} w={3} rounded="full" bg="red.500" />
-      )}
+      <Box h={3} w={3} rounded="full" {...statusStyles[status]} />
     </Tooltip>
   );
 };
