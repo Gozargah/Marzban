98ee34fb61bc7f26bd25401901ea8d95ce3d0267
Random Guy <50927468+M03ED@users.noreply.github.com>
2024-04-28 21:12:53 +0330

    feat: add json config for v2rayn

--- app/views/subscription.py
+++ app/views/subscription.py
@@ -98,7 +98,16 @@ def user_subscription(token: str,
             return Response(content=conf, media_type="application/json", headers=response_headers)
         else:
             conf = generate_subscription(user=user, config_format="v2ray", as_base64=True)
+            return Response(content=conf, media_type="text/plain", headers=response_headers)
+
+    elif re.match('^v2rayN/(\d+\.\d+)', user_agent):
+        version_str = re.match('^v2rayN/(\d+\.\d+)', user_agent).group(1)
+        if LooseVersion(version_str) >= LooseVersion("6.40"):
+            conf = generate_subscription(user=user, config_format="v2ray-json", as_base64=False)
             return Response(content=conf, media_type="application/json", headers=response_headers)
+        else:
+            conf = generate_subscription(user=user, config_format="v2ray", as_base64=True)
+            return Response(content=conf, media_type="text/plain", headers=response_headers)
 
     else:
         conf = generate_subscription(user=user, config_format="v2ray", as_base64=True)
