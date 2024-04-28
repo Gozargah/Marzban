41ae408e843a95cad93c7ce5d5178b6b1aa02a72
Random Guy <50927468+M03ED@users.noreply.github.com>
2024-04-29 00:04:52 +0330

    fix: status change for not on hold users

--- app/jobs/review_users.py
+++ app/jobs/review_users.py
@@ -55,10 +55,8 @@ def review():
             xray.operations.remove_user(user)
             update_user_status(db, user, status)
 
-            bg.add_task(
-                report.status_change, username=user.username, status=status,
-                user=UserResponse.from_orm(user), user_admin=user.admin
-            )
+            report.status_change(username=user.username, status=status,
+                user=UserResponse.from_orm(user), user_admin=user.admin)
 
             logger.info(f"User \"{user.username}\" status changed to {status}")
 
