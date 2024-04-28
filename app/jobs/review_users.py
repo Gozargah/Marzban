cff6f6753663ad6e88df30dc10b882a83c5649db
Random Guy <50927468+M03ED@users.noreply.github.com>
2024-04-28 15:02:49 +0330

    fix: report change status temporary

--- app/jobs/review_users.py
+++ app/jobs/review_users.py
@@ -82,10 +82,9 @@ def review():
 
             update_user_status(db, user, status)
             start_user_expire(db, user)
-            bg.add_task(
-                report.status_change, username=user.username, status=status,
-                user=UserResponse.from_orm(user), user_admin=user.admin
-            )
+            
+            report.status_change(username=user.username, status=status,
+                user=UserResponse.from_orm(user), user_admin=user.admin)
 
             logger.info(f"User \"{user.username}\" status changed to {status}")
 
