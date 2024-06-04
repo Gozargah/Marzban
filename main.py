3965b409d53855c7bd71a42d8d23fce8fd89af4e
fodhelper <161948085+fodhelper@users.noreply.github.com>
2024-06-04 21:51:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ if __name__ == "__main__":
     try:
         uvicorn.run(
             "main:app",
-            host=('0.0.0.0' if DEBUG else UVICORN_HOST),
+            host=UVICORN_HOST,
             port=UVICORN_PORT,
-            uds=(None if DEBUG else UVICORN_UDS),
+            uds=UVICORN_UDS,
             ssl_certfile=UVICORN_SSL_CERTFILE,
             ssl_keyfile=UVICORN_SSL_KEYFILE,
             workers=1,
