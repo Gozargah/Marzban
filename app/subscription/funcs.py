f1c0d34bcfc22e76e8b10246019c705add1af3bc
fodhelper <161948085+fodhelper@users.noreply.github.com>
2024-07-05 12:31:14 +0100

    Update funcs.py

--- app/subscription/funcs.py
+++ app/subscription/funcs.py
@@ -18,3 +18,25 @@ def get_grpc_multi(path: str) -> str:
     streamname = path.rsplit("/", 1)[1].split("|")[1]
 
     return "%s%s%s" % (servicename, "/", streamname)
+
+def get_grpc_gun_ng(path: str) -> str:
+    if path == "":
+        return "/Tun"
+    if not path.startswith("/"):
+        return "%s%s%s" % ("/", path, "/Tun")
+
+    servicename = path.rsplit("/", 1)[0]
+    streamname = path.rsplit("/", 1)[1].split("|")[0]
+
+    return "%s%s%s" % (servicename, "/", streamname)
+
+def get_grpc_multi_ng(path: str) -> str:
+    if path == "":
+        return "/TunMulti"
+    if not path.startswith("/"):
+        return "%s%s%s" % ("/", path, "/TunMulti")
+    
+    servicename = path.rsplit("/", 1)[0]
+    streamname = path.rsplit("/", 1)[1].split("|")[1]
+
+    return "%s%s%s" % (servicename, "/", streamname)
