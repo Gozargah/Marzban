0b06841ff470fac1ad806a049777a88ee92c270c
Mohammad20000 <imMohammad20000@ProtonMail.com>
2024-07-19 10:04:52 +0330

    fix: clash duplicated names

--- app/subscription/clash.py
+++ app/subscription/clash.py
@@ -6,10 +6,10 @@ import yaml
 from app.subscription.funcs import get_grpc_gun
 from app.templates import render_template
 from config import (
-    CLASH_SUBSCRIPTION_TEMPLATE, 
-    MUX_TEMPLATE,
-    USER_AGENT_TEMPLATE,
+    CLASH_SUBSCRIPTION_TEMPLATE,
     GRPC_USER_AGENT_TEMPLATE,
+    MUX_TEMPLATE,
+    USER_AGENT_TEMPLATE
 )
 
 
@@ -94,6 +94,7 @@ class ClashConfiguration(object):
             network = 'http'
 
         remark = self._remark_validation(name)
+        self.proxy_remarks.append(remark)
         node = {
             'name': remark,
             'type': type,
@@ -208,18 +209,15 @@ class ClashConfiguration(object):
             node['alterId'] = 0
             node['cipher'] = 'auto'
             self.data['proxies'].append(node)
-            self.proxy_remarks.append(remark)
 
         if inbound['protocol'] == 'trojan':
             node['password'] = settings['password']
             self.data['proxies'].append(node)
-            self.proxy_remarks.append(remark)
 
         if inbound['protocol'] == 'shadowsocks':
             node['password'] = settings['password']
             node['cipher'] = settings['method']
             self.data['proxies'].append(node)
-            self.proxy_remarks.append(remark)
 
 
 class ClashMetaConfiguration(ClashConfiguration):
@@ -293,7 +291,6 @@ class ClashMetaConfiguration(ClashConfiguration):
             node['alterId'] = 0
             node['cipher'] = 'auto'
             self.data['proxies'].append(node)
-            self.proxy_remarks.append(remark)
 
         if inbound['protocol'] == 'vless':
             node['uuid'] = settings['id']
@@ -302,7 +299,6 @@ class ClashMetaConfiguration(ClashConfiguration):
                 node['flow'] = settings.get('flow', '')
 
             self.data['proxies'].append(node)
-            self.proxy_remarks.append(remark)
 
         if inbound['protocol'] == 'trojan':
             node['password'] = settings['password']
@@ -311,10 +307,8 @@ class ClashMetaConfiguration(ClashConfiguration):
                 node['flow'] = settings.get('flow', '')
 
             self.data['proxies'].append(node)
-            self.proxy_remarks.append(remark)
 
         if inbound['protocol'] == 'shadowsocks':
             node['password'] = settings['password']
             node['cipher'] = settings['method']
             self.data['proxies'].append(node)
-            self.proxy_remarks.append(remark)
