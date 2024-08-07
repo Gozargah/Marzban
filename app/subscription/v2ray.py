eb84a09da41ec9266080366d5cb6715bebde2400
Mohammad20000 <imMohammad20000@ProtonMail.com>
2024-08-07 13:20:18 +0330

    fix: change outbound tag to proxy in v2ray custom config

--- app/subscription/v2ray.py
+++ app/subscription/v2ray.py
@@ -864,7 +864,7 @@ class V2rayJsonConfig(str):
                 path = get_grpc_gun(path)
 
         outbound = {
-            "tag": remark,
+            "tag": "proxy",
             "protocol": protocol
         }
 
