75ddfb1b088df67236047cd831145382e2f3d438
Mohammad20000 <imMohammad20000@ProtonMail.com>
2024-09-03 23:44:49 +0330

    set add_as_new_host default value to false

--- app/dashboard/src/components/NodesModal.tsx
+++ app/dashboard/src/components/NodesModal.tsx
@@ -249,7 +249,7 @@ const AddNodeForm: FC<AddNodeFormType> = ({
     resolver: zodResolver(NodeSchema),
     defaultValues: {
       ...getNodeDefaultValues(),
-      add_as_new_host: true,
+      add_as_new_host: false,
     },
   });
   const { isLoading, mutate } = useMutation(addNode, {
