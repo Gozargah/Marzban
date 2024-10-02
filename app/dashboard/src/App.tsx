322a16ed12df600cd1c7313fa3ba971dbcec8500
iambabyninja <iamkorotaev228@icloud.com>
2024-10-02 19:41:56 +0500

    dynamic dashboard path

--- app/dashboard/src/App.tsx
+++ app/dashboard/src/App.tsx
@@ -1,7 +1,7 @@
 import "react-datepicker/dist/react-datepicker.css";
 import "react-loading-skeleton/dist/skeleton.css";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./components/modules/Router";
+import { router } from "./pages/Router";
 
 function App() {
   return (
