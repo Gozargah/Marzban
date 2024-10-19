d6fde339099af33518e3bd49b2415d8751dc9ee9
John Doe <govfvck@proton.me>
2024-10-19 15:08:17 +0330

    remove fastapi_responses, add manual additional responses docs

---
+++ app/utils/responses.py
@@ -0,0 +1,43 @@
+"""Documented Error responses for API routes"""
+
+from pydantic import BaseModel
+
+
+class HTTPException(BaseModel):
+    detail: str
+
+
+class Unauthorized(HTTPException):
+    detail: str = "Not authenticated"
+
+
+class Forbidden(HTTPException):
+    detail: str = "You're not allowed 'to ...'"
+
+
+class NotFound(HTTPException):
+    detail: str = "'Entity' '{}' not found"
+
+
+class Conflict(HTTPException):
+    detail: str = "'Entity' already exists"
+
+
+_400 = {"description": "Bad request", "model": HTTPException}
+
+_401 = {
+    "description": "Unauthorized",
+    "model": Unauthorized,
+    "headers": {
+        "WWW-Authenticate": {
+            "description": "Authentication type",
+            "type": "string",
+        },
+    },
+}
+
+_403 = {"description": "Forbidden", "model": Forbidden}
+
+_404 = {"description": "Not found", "model": NotFound}
+
+_409 = {"description": "Conflict", "model": Conflict}
