4d23639219bf2a7a6f0a706279288df0d724b3fa
Mohammad <imMohammad20000@ProtonMail.com>
2024-12-12 17:03:20 +0330

    remove unnesserly code

--- app/db/migrations/versions/e7b869e999b4_increase_length_of_the_host_and_sni_.py
+++ app/db/migrations/versions/e7b869e999b4_increase_length_of_the_host_and_sni_.py
@@ -17,50 +17,25 @@ depends_on = None
 
 def upgrade():
     # Increase the length of 'sni' and 'host' columns to 1000
-    bind = op.get_bind()
-    if bind.engine.name == 'sqlite':
-        with op.batch_alter_table('hosts') as batch_op:
-            batch_op.alter_column('host',
-                                  existing_type=sa.String(length=256),
-                                  type_=sa.String(length=1000),
-                                  nullable=True)
-            batch_op.alter_column('sni',
-                                  existing_type=sa.String(length=256),
-                                  type_=sa.String(length=1000),
-                                  nullable=True)
-
-    else:
-        op.alter_column('hosts', 'sni',
-                        existing_type=sa.String(length=256),  # old length or assumed type
-                        type_=sa.String(length=1000),        # new length
-                        existing_nullable=True)             # nullable or not
-
-        op.alter_column('hosts', 'host',
-                        existing_type=sa.String(length=256),  # old length or assumed type
-                        type_=sa.String(length=1000),        # new length
-                        existing_nullable=True)
+    with op.batch_alter_table('hosts') as batch_op:
+        batch_op.alter_column('host',
+                              existing_type=sa.String(length=256),
+                              type_=sa.String(length=1000),
+                              nullable=True)
+        batch_op.alter_column('sni',
+                              existing_type=sa.String(length=256),
+                              type_=sa.String(length=1000),
+                              nullable=True)
 
 
 def downgrade():
     # Revert the column lengths back to their original size
-    bind = op.get_bind()
-    if bind.engine.name == 'sqlite':
-        with op.batch_alter_table('hosts') as batch_op:
-            batch_op.alter_column('host',
-                                  existing_type=sa.String(length=1000),
-                                  type_=sa.String(length=256),
-                                  nullable=True)
-            batch_op.alter_column('sni',
-                                  existing_type=sa.String(length=1000),
-                                  type_=sa.String(length=256),
-                                  nullable=True)
-
-    op.alter_column('hosts', 'sni',
-                    existing_type=sa.String(length=1000),
-                    type_=sa.String(length=256),
-                    existing_nullable=True)
-
-    op.alter_column('hosts', 'host',
-                    existing_type=sa.String(length=1000),
-                    type_=sa.String(length=256),
-                    existing_nullable=True)
+    with op.batch_alter_table('hosts') as batch_op:
+        batch_op.alter_column('host',
+                              existing_type=sa.String(length=1000),
+                              type_=sa.String(length=256),
+                              nullable=True)
+        batch_op.alter_column('sni',
+                              existing_type=sa.String(length=1000),
+                              type_=sa.String(length=256),
+                              nullable=True)
