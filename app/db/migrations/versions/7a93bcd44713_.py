6a28e58544ac659fc83472b003c72435e4d6b7f4
Mohammad <imMohammad20000@ProtonMail.com>
2025-01-11 12:36:32 +0330

    fix: migrations conflicts (#1595)

---
+++ app/db/migrations/versions/7a93bcd44713_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 7a93bcd44713
+Revises: 2b231de97dc3, f2b9caa23e16
+Create Date: 2025-01-11 12:30:03.803844
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7a93bcd44713'
+down_revision = ('2b231de97dc3', 'f2b9caa23e16')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
