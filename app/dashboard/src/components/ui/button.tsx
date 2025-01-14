769f5dbebc67ab90dd7e2fa22a068e611beba268
Gozarman <120019621+gozarman@users.noreply.github.com>
2025-01-14 09:19:54 +0000

    fix: button styles

--- app/dashboard/src/components/ui/button.tsx
+++ app/dashboard/src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { LoaderCircleIcon } from 'lucide-react'
 import * as React from 'react'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 gap-1 items-center',
   {
     variants: {
       variant: {
@@ -18,7 +18,7 @@ const buttonVariants = cva(
       },
       size: {
         default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
+        sm: 'h-9 rounded-md px-3 [&>svg]:w-5 [&>svg]:h-5',
         lg: 'h-11 rounded-md px-8',
         icon: 'h-7 w-7 [&>svg]:w-4 [&>svg]:h-4 [&>svg]:stroke-[1.5px] rounded-sm',
       },
