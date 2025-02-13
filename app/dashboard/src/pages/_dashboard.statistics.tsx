315004a6ee9a73d1b8d827ffbbc48cd3906fa8a9
Sinacket <bulletsina77@gmail.com>
2025-02-13 14:34:11 +0330

    feat: statistics header filled with dynamic data and translated

--- app/dashboard/src/pages/_dashboard.statistics.tsx
+++ app/dashboard/src/pages/_dashboard.statistics.tsx
@@ -1,14 +1,22 @@
 import PageHeader from '@/components/page-header'
 import MainContent from '@/components/statistics/Statistics'
 import { Separator } from '@/components/ui/separator'
+import { getSystemStats } from '@/service/api'
+import { useQuery } from '@tanstack/react-query'
 
 const Statistics = () => {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["systemStats"],
+    queryFn: () => getSystemStats(),
+    refetchInterval: 5000
+  });
+
   return (
     <div className="flex flex-col gap-2 w-full items-start">
       <PageHeader title="statistics" description="monitorServers" />
       <Separator />
       <div className="px-4 w-full pt-2">
-        <MainContent />
+        <MainContent error={error} isLoading={isLoading} data={data} />
       </div>
     </div>
   )
--- app/dashboard/src/utils/formatByte.ts
+++ app/dashboard/src/utils/formatByte.ts
@@ -1,16 +1,23 @@
-export function formatBytes(bytes: number, decimals = 2, asArray = false) {
-  if (!+bytes) return "0 B";
+export function formatBytes(
+  bytes: number,
+  decimals = 2,
+  size: boolean = true,
+  asArray = false
+) {
+  if (!+bytes) return size ? "0 B" : "0";
 
   const k = 1024;
   const dm = decimals < 0 ? 0 : decimals;
   const sizes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
 
   const i = Math.floor(Math.log(bytes) / Math.log(k));
-  if (!asArray)
-    return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
-  else return [parseFloat((bytes / Math.pow(k, i)).toFixed(dm)), sizes[i]];
+  const value = parseFloat((bytes / Math.pow(k, i)).toFixed(dm));
+
+  if (asArray) return [value, sizes[i]];
+  return size ? `${value} ${sizes[i]}` : `${value}`;
 }
 
 export const numberWithCommas = (x: number) => {
-  if (x !== null) return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+if (x !== null) return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
+
