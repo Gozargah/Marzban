c53ac1b0ee109ed6b4ffee2d67bd36192db49ef8
eso <134785284+esodrevo@users.noreply.github.com>
2025-02-18 05:14:27 -0800

    Update subscription.py
    npv tunnel

--- app/routers/subscription.py
+++ app/routers/subscription.py
@@ -17,6 +17,7 @@ from config import (
     USE_CUSTOM_JSON_DEFAULT,
     USE_CUSTOM_JSON_FOR_HAPP,
     USE_CUSTOM_JSON_FOR_STREISAND,
+    USE_CUSTOM_JSON_FOR_NPVTUNNEL,
     USE_CUSTOM_JSON_FOR_V2RAYN,
     USE_CUSTOM_JSON_FOR_V2RAYNG,
     XRAY_SUBSCRIPTION_PATH,
@@ -132,8 +133,16 @@ def user_subscription(
             conf = generate_subscription(user=user, config_format="v2ray", as_base64=True, reverse=False)
             return Response(content=conf, media_type="text/plain", headers=response_headers)
 
+    elif USE_CUSTOM_JSON_DEFAULT or USE_CUSTOM_JSON_FOR_NPVTUNNEL:
+        if "ktor-client" in user_agent:
+            conf = generate_subscription(user=user, config_format="v2ray-json", as_base64=False, reverse=False)
+            return Response(content=conf, media_type="application/json", headers=response_headers)
+        else:
+            conf = generate_subscription(user=user, config_format="v2ray", as_base64=True, reverse=False)
+            return Response(content=conf, media_type="text/plain", headers=response_headers)
 
 
+    
     else:
         conf = generate_subscription(user=user, config_format="v2ray", as_base64=True, reverse=False)
         return Response(content=conf, media_type="text/plain", headers=response_headers)
