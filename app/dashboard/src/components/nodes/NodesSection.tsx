7379ed66a5827423f4f233f3ba9d3308179f4fa9
mohammadmahdi mohammadi <mohammadimahdi294@gmail.com>
2025-02-20 02:22:14 +0330

    add editNodeModal , add certificate from node setting , add certificate to addNodeModal and dashboard.nodes

--- app/dashboard/src/components/nodes/NodesSection.tsx
+++ app/dashboard/src/components/nodes/NodesSection.tsx
@@ -1,20 +1,24 @@
-import { NodeType } from "@/contexts/NodesContext";
+import {NodeType} from "@/contexts/NodesContext";
 import useDirDetection from "@/hooks/use-dir-detection";
 import Node from "./Node";
+import {NodeSettings} from "@/service/api";
 
-const NodesSection = ({ nodes }: { nodes: NodeType[] | undefined }) => {
-  const dir = useDirDetection();
+const NodesSection = ({nodes, nodeSetting}: {
+    nodes: NodeType[] | undefined,
+    nodeSetting: NodeSettings | undefined
+}) => {
+    const dir = useDirDetection();
 
-  return (
-    <div
-      dir={dir}
-      className="px-8 py-10 gap-4 lg:gap-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3"
-    >
-      {nodes?.map((node: NodeType) => (
-        <Node node={node} key={node.id} />
-      ))}
-    </div>
-  );
+    return (
+        <div
+            dir={dir}
+            className="px-8 py-10 gap-4 lg:gap-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3"
+        >
+            {nodes?.map((node: NodeType) => (
+                <Node nodeSetting={nodeSetting} node={node} key={node.id}/>
+            ))}
+        </div>
+    );
 };
 
 export default NodesSection;
