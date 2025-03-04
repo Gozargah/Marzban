e8626cb95baa9146bff106b721c8ed1825a863da
Mohammad <imMohammad20000@ProtonMail.com>
2025-03-04 13:44:30 +0330

    refactor: reality settings key handling
    - Removed redundant try-except block for publicKey retrieval.
    - remove depending on xray-core for get x25519 publickey and replaced`
    core.get_x25519` with `get_x25519_public_key` method

--- app/utils/crypto.py
+++ app/utils/crypto.py
@@ -1,5 +1,10 @@
+import base64
+import binascii
+
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import x25519
 from OpenSSL import crypto
 
 
@@ -24,10 +29,57 @@ def generate_certificate():
     cert.set_issuer(cert.get_subject())
     cert.set_pubkey(k)
     cert.sign(k, 'sha512')
-    cert_pem = crypto.dump_certificate(crypto.FILETYPE_PEM, cert).decode("utf-8")
+    cert_pem = crypto.dump_certificate(
+        crypto.FILETYPE_PEM, cert).decode("utf-8")
     key_pem = crypto.dump_privatekey(crypto.FILETYPE_PEM, k).decode("utf-8")
 
     return {
         "cert": cert_pem,
         "key": key_pem
     }
+
+
+def add_base64_padding(b64_string: str) -> str:
+    """Adds missing Base64 padding if necessary."""
+    missing_padding = len(b64_string) % 4
+    return b64_string + ('=' * (4 - missing_padding)) if missing_padding else b64_string
+
+
+def get_x25519_public_key(private_key_b64: str) -> str:
+    """
+    Converts an X25519 private key (URL-safe Base64) into a public key (URL-safe Base64 format).
+
+    :param private_key_b64: The private key in URL-safe Base64 format (without padding).
+    :return: The corresponding public key as a URL-safe Base64 string (without padding).
+    """
+    try:
+        # Decode Base64 (URL-safe) Add padding if needed
+        private_key_bytes = base64.urlsafe_b64decode(
+            add_base64_padding(private_key_b64))
+
+        # Ensure the private key is 32 bytes
+        if len(private_key_bytes) != 32:
+            raise ValueError(
+                "Invalid private key length. Must be 32 bytes after decoding.")
+
+        # Load the private key
+        private_key = x25519.X25519PrivateKey.from_private_bytes(
+            private_key_bytes)
+
+        # Derive the public key
+        public_key = private_key.public_key()
+
+        # Convert the public key to bytes
+        public_key_bytes = public_key.public_bytes(
+            encoding=serialization.Encoding.Raw,
+            format=serialization.PublicFormat.Raw
+        )
+
+        # Encode the public key as URL-safe Base64 (without padding)
+        public_key_b64 = base64.urlsafe_b64encode(
+            public_key_bytes).decode().rstrip("=")
+
+        return public_key_b64
+
+    except (ValueError, binascii.Error):
+        raise ValueError("Invalid private key.")
