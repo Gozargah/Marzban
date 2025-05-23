e4be2365ad1f7113c89b43a588f1341262f98ad5
z0roday <100026917+z0roday@users.noreply.github.com>
2025-05-23 02:09:45 -0700

    رفع خطای تعاریف متفاوت Window در TypeScript

--- app/dashboard/src/vite-env.d.ts
+++ app/dashboard/src/vite-env.d.ts
@@ -2,9 +2,9 @@
 /// <reference types="vite-plugin-svgr/client" />
 
 interface Window {
-    BASE_API: string;
-    DASHBOARD_TITLE: string;
-    DASHBOARD_LOGO: string;
-    DASHBOARD_THEME_COLOR: string;
-    DASHBOARD_DARK_MODE: string;
+    BASE_API?: string;
+    DASHBOARD_TITLE?: string;
+    DASHBOARD_LOGO?: string;
+    DASHBOARD_THEME_COLOR?: string;
+    DASHBOARD_DARK_MODE?: string;
 }
