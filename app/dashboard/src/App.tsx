e07c828d0e445609a2825a91aec2e6bcf497d99c
z0roday <100026917+z0roday@users.noreply.github.com>
2025-05-23 01:58:22 -0700

    بهبود عنوان صفحه داشبورد با استفاده از متغیر محیطی

--- app/dashboard/src/App.tsx
+++ app/dashboard/src/App.tsx
@@ -2,8 +2,15 @@ import "react-datepicker/dist/react-datepicker.css";
 import "react-loading-skeleton/dist/skeleton.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./pages/Router";
+import { useEffect } from "react";
 
 function App() {
+    useEffect(() => {
+        if (window.DASHBOARD_TITLE) {
+            document.title = window.DASHBOARD_TITLE;
+        }
+    }, []);
+
     return (
         <main className="p-8">
             <RouterProvider router={router} />
--- app/dashboard/src/vite-env.d.ts
+++ app/dashboard/src/vite-env.d.ts
@@ -1,2 +1,10 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
+
+interface Window {
+    BASE_API: string;
+    DASHBOARD_TITLE: string;
+    DASHBOARD_LOGO: string;
+    DASHBOARD_THEME_COLOR: string;
+    DASHBOARD_DARK_MODE: string;
+}
